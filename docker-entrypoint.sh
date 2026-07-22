79f1bd72059a92ba99f4e2892bde7f0a8452116b
cristhian-sancore <cristhiansancore@gmail.com>
2026-07-22 10:15:50 -0400

    fix: permissoes do banco e volumes sqlite no entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,5 +9,9 @@ if [ ! -f /app/data/dev.db ]; then
   cp /app/dev.db /app/data/dev.db
 fi
 
+# Dá permissão total para evitar o Erro 14 do SQLite
+chmod 777 /app/data
+chmod 666 /app/data/dev.db
+
 # Inicia a aplicação
 exec node server.js
