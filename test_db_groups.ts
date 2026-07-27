3fd719f9bc4de43465d2c7ac3713c89106376287
cristhian-sancore <cristhiansancore@gmail.com>
2026-07-27 14:03:11 -0400

    fix: remove scripts de teste que estavam quebrando o build do NextJS por colisao de nomes globais

--- test_db_groups.ts
+++
@@ -1,6 +0,0 @@
-import prisma from './src/lib/prisma';
-async function main() {
-  const groups = await prisma.accessGroup.findMany({ select: { id: true, name: true, glpiGroupId: true } });
-  console.log(groups);
-}
-main();
